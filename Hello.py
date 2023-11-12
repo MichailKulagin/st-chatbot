3658f139432cf8cc260d06ee2303247e45bcb34f
MichailKulagin <112321836+MichailKulagin@users.noreply.github.com>
2023-11-12 16:13:48 +0300

    Update Hello.py
    исправление кода

--- Hello.py
+++ Hello.py
@@ -1,51 +1,103 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+import g4f
 
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+prompt = ""
 
 
-if __name__ == "__main__":
-    run()
+
+st.title("💬 Chatbot бот от Михалыча")
+st.caption("🚀 Чат бот готов к вашим запросам")
+
+recognition = st.toggle('Выкл/Вкл записи микрофона')
+
+
+# User-provided prompt
+if prompt := st.chat_input():
+    pass
+
+
+
+stt_button = Button(label="Запись с микрофона", width=100, disabled=False, button_type="primary")
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
+
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    if "GET_TEXT" in result:
+        #st.write(result.get("GET_TEXT"))
+        if recognition:
+            prompt = result.get("GET_TEXT")
+            
+        
+
+# Хранить сгенерированные LLM ответы
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Привет! Я искусственный интеллект, созданный для помощи в различных задачах. Я могу отвечать на вопросы, предоставлять информацию, помогать в поиске решений, переводить тексты на разные языки, создавать напоминания и многое другое. Если у тебя есть какие-либо конкретные вопросы или задачи, я постараюсь помочь!"}]
+
+
+if prompt == None:
+    pass
+else:
+    print(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    
+
+
+# Отображение или удаление сообщений чата
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Generating..."):
+            print("Prompt: ", prompt)
+            #st.chat_message("assistant").write("Generating...")
+            response = g4f.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content": f"{prompt}"}],
+        )
+            print("Response: ", response)
+            prompt = ""
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
+
+
+
+
+
